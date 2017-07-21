8af4c120ce4f09ed463914be2fb227f9a5d5e0e8
Shilenkov <vshilenk@visteon.com>
2017-07-21 12:49:11 +0200

    acm.timus 1000

---
+++ acm.timus.ru/00001000/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Вычислите a+b
+ *
+ *  @see http://acm.timus.ru/problem.aspx?space=1&num=1000
+ *
+ */ 
+
+#include <cstdio>
+
+int main()
+{
+    long int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a+b);
+    return 0;
+}
