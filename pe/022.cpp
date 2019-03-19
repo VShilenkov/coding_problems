515998267acbbdd24b6d02632a442368a7cc4830
Shilenkov <vshilenkov@hotmail.com>
2019-03-19 12:24:54 +0100

    022. solved

--- pe/022.cpp
+++ pe/022.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <map>
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc != 2U) { return 1; }
+
+    std::ifstream input;
+    input.open(argv[1]);
+    if (!input) { return 1; }
+
+    std::map<std::string, unsigned int> d;
+    std::string                         name;
+    while (input >> name)
+    {
+        unsigned score = 0U;
+        for (char c : name) { score += (c - 'A') + 1U; }
+        d[name] = score;
+    }
+
+    size_t   index = 1U;
+    unsigned sum   = 0U;
+    for (auto a = d.cbegin( ); a != d.cend( ); ++a)
+    {
+        unsigned score = index * a->second;
+        sum += score;
+        ++index;
+    }
+    std::cout << sum;
     return 0;
 }
