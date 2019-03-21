a639f1161d00ea3be662f25ddf3b0351cbc22213
Vitalii Shylienkov <vshilenkov@hotmail.com>
2019-03-21 14:55:32 +0100

    024. small updates

--- pe/024.cpp
+++ pe/024.cpp
@@ -6,29 +6,26 @@ void naive(unsigned permutation_number)
 {
     unsigned a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    for (unsigned i = 0; i < permutation_number; ++i)
-    {
-        std::next_permutation(a, a + 10U);
-        // std::cout << std::endl;
-    }
+    for (unsigned i = 0; i < permutation_number; ++i) { std::next_permutation(a, a + 10U); }
     for (unsigned c : a) { std::cout << c; }
     std::cout << std::endl;
 }
 
 void factorial_representaion(unsigned permutation_number)
 {
-    unsigned factor_representation[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    constexpr unsigned base                        = 10U;
+    unsigned           factor_representation[base] = {0};
 
-    for (unsigned i = 1U; i < 11U; ++i)
+    for (unsigned i = 2U; i <= base; ++i)
     {
-        factor_representation[10U - i] = permutation_number % i;
+        factor_representation[base - i] = permutation_number % i;
         permutation_number /= i;
     }
 
     std::list<unsigned> permutation = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (unsigned i = 0U; i < 10U; ++i)
+    for (unsigned i = 0U; i < base; ++i)
     {
-        auto p = std::next(permutation.begin( ), factor_representation[i]);
+        std::list<unsigned>::iterator p = std::next(permutation.begin( ), factor_representation[i]);
         std::cout << *p;
         permutation.erase(p);
     }
