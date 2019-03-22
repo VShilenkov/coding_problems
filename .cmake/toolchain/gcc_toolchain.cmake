fc12e55b4aa363f6d4dda9ce4829e5b6e95cb90a
Vitalii Shylienkov <vshilenkov@hotmail.com>
2019-03-22 14:45:22 +0100

    setup clang as a package in msys2. adaption

--- .cmake/toolchain/gcc_toolchain.cmake
+++ .cmake/toolchain/gcc_toolchain.cmake
@@ -4,11 +4,12 @@ set(CMAKE_C_COMPILER_ID   "GNU" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER    "g++" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER_ID "GNU" CACHE STRING "" FORCE)
 
-set(CMAKE_LINKER          "ld"  CACHE STRING "" FORCE)
+set(CMAKE_LINKER "ld"  CACHE STRING "" FORCE)
 
 if (NOT WIN32)
     set(CMAKE_EXECUTABLE_SUFFIX_CXX ".out" CACHE STRING "" FORCE)
     set(CMAKE_EXECUTABLE_SUFFIX_C   ".out" CACHE STRING "" FORCE)
 endif(NOT WIN32)
 
-add_compile_options("SHELL:-Wall -Wpedantic -Wextra")
+add_compile_options("SHELL:-Wall -Wextra -Wpedantic")
+add_compile_options("SHELL:-pedantic -fdiagnostics-show-option")
