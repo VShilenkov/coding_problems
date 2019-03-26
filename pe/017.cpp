756216982bbca71b88493e46fa2de06b2856b950
Vitalii Shylienkov <vshilenkov@hotmail.com>
2019-03-26 11:41:30 +0100

    017. doc

--- pe/017.cpp
+++ pe/017.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
+
 using T = unsigned int;
 
-int main( )
-{
-    constexpr T ones[]  = {0U, 3U, 3U, 5U, 4U, 4U, 3U, 5U, 5U, 4U};
-    constexpr T teens[] = {0U, 6U, 6U, 8U, 8U, 7U, 7U, 9U, 8U, 8U};
-    constexpr T tens[]  = {0U, 3U, 6U, 6U, 5U, 5U, 5U, 7U, 6U, 6U};
+constexpr T ones[]  = {0U, 3U, 3U, 5U, 4U, 4U, 3U, 5U, 5U, 4U};
+constexpr T teens[] = {0U, 6U, 6U, 8U, 8U, 7U, 7U, 9U, 8U, 8U};
+constexpr T tens[]  = {0U, 3U, 6U, 6U, 5U, 5U, 5U, 7U, 6U, 6U};
 
+constexpr T H  = 7U;    // "hundred" length
+constexpr T A  = 3U;    // "and" length
+constexpr T Th = 8U;    // "thousand" length
+
+inline void naive( )
+{
     constexpr T base              = 10U;
     constexpr T hundred           = base * base;
     constexpr T teens_lower_limit = base;
@@ -22,13 +27,35 @@ int main( )
                                                                   : (tens[i / base] + ones[i % base]));
     }
 
-    constexpr T hundred_word_length = 7U;
-    constexpr T and_word_lenth      = 3U;
-    constexpr T prefix              = hundred_word_length + and_word_lenth;
-
-    T sum_total = 11U + base * sum_first_ninty_nine - (base - 1U) * and_word_lenth;
+    T sum_total = ones[1] + Th + base * sum_first_ninty_nine - (base - 1U) * A;
     // onethousand_word_lenth + (sum till hundred for each hundred) - and_word_lenth for 100, 200, 300,...,900
     for (T i = 1U; i < base; ++i)
-    { sum_total += (ones[i] + prefix) * hundred; }    // go trough each hundred 100, 200, ..., 900
+    {
+        sum_total += (ones[i] + H + A) * hundred;    // go trough each hundred 100, 200, ..., 900
+    }
     std::cout << sum_total;
+}
+
+inline void algo( )
+{
+    T sum_ones = 0U, sum_teens = 0U, sum_tens = 0U;
+
+    for (T i = 1U; i < 10U; ++i)
+    {
+        sum_ones += ones[i];
+        sum_teens += teens[i];
+        sum_tens += tens[i];
+    }
+
+    sum_tens -= tens[1];
+
+    std::cout << 10 * (19 * sum_ones + tens[1] + sum_teens + 10 * sum_tens) + 9 * (100 * (H + A) - A) + ones[1] + Th;
+}
+
+int main( )
+{
+    naive( );
+    std::cout << std::endl;
+    algo( );
+    return 0;
 }
